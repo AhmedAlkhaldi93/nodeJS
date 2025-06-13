9142238dba507d768821070e95834fc7d6787f69
AhmedAlkhaldi93 <ahmed.el-khaldi@hotmail.com>
2025-06-13 23:52:49 +0200

    a

--- nodeJS-week1/homework-week1/avg.js
+++ nodeJS-week1/homework-week1/avg.js
@@ -1,13 +1,1 @@
 
-const args = process.argv.slice(2);
-
-if(args.length === 0){
-    console.log("Please enter your numbers");
-}else{
-    var sum = 0;
-    for(let i= 0; i < args.length; i++){
-        sum = sum + Number(args[i]);
-    }
-    const avg = sum/args.length;
-    console.log(avg);
-}
