b9b94f671b86268817da7bcd55957e140cb374b8
AhmedAlkhaldi93 <ahmed.el-khaldi@hotmail.com>
2025-06-16 13:13:52 +0200

    Upload the warmup homework (week3)

--- nodeJS-week1/homework-week1/app.js
+++ nodeJS-week1/homework-week1/app.js
@@ -1,11 +1,63 @@
 import express from 'express'
 import documents from './documents.json' with { type: "json" };
+import knex from 'knex'
+
+const knexInstance = knex({
+  client: "mysql2",
+  connection: {
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "114519",
+    database: process.env.DB_NAME || "hyf_node_week3_warmup",
+    multipleStatements: true,
+  },
+});
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Support parsing JSON requests
 app.use(express.json());
+
+const apiRouter = express.Router();
+app.use("/api", apiRouter);
+
+const contactsAPIRouter = express.Router();
+apiRouter.use("/contacts", contactsAPIRouter);
+
+contactsAPIRouter.get("/", async (req, res) => {            //http://localhost:3000/api/contacts
+  let query = knexInstance.select("*").from("contacts");
+  const columnInfo = await knexInstance("contacts").columnInfo();
+
+  const columnNames = Object.keys(columnInfo);
+  const allowedDirections = ["asc", "desc"];
+
+
+  if ("sort" in req.query) {
+    const orderBy = req.query.sort.toLowerCase().split(" ");
+    const column = orderBy[0];
+    const direction = orderBy[1] || "asc";
+
+    if(columnNames.includes(column) && allowedDirections.includes(direction)){
+      const sortQuery = column + " " + direction;
+      query = query.orderByRaw(sortQuery);
+    }else{
+      return res.status(400).json({ error: "Invalid sort parameters" });
+    }
+  }
+
+  console.log("SQL", query.toSQL().sql);
+
+  try {
+    const data = await query;
+    res.json({ data });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 
 app.get("/search", (req, res) => {
   const search = req.query.q;
