45ae1ebedf369f16e6c3999ea52dd43356a44ab4
AhmedAlkhaldi93 <ahmed.el-khaldi@hotmail.com>
2025-06-21 17:10:25 +0200

    updade nodeJS-wee1

--- nodeJS-week1/homework-week1/avg.js
+++ nodeJS-week1/homework-week1/avg.js
@@ -3,10 +3,14 @@ const args = process.argv.slice(2);
 if(args.length === 0){
     console.log("Please enter your numbers");
 }else{
-    
     var sum = 0;
+    
     for(let i= 0; i < args.length; i++){
         sum = sum + Number(args[i]);
+        if (isNaN(sum)) {
+            console.log(Invalid number: "${args[i]}");
+            process.exit(1);
+        }
     }
     const avg = sum/args.length;
     console.log(avg);
